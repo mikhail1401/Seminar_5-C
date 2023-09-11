f7a8c3e71818f21e909d894cd61271e08a3d37a6
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2023-09-11 01:25:01 -0400

    No changes

--- Program.cs
+++ Program.cs
@@ -50,7 +50,7 @@ for (int i=0; i<array2.Length; i++)
 }
 
 
-Console.WriteLine("\nTask 33");
+Console.WriteLine("\n\nTask 33");
 // Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // -3; массив [6, 7, 19, 345, 3] -> да
@@ -75,3 +75,32 @@ for (int i=0; i<array3.Length; i++)
 
 if (numIsPresent==true) Console.WriteLine($"\nYes, number {num} is present in the array.");
 else Console.WriteLine($"\nNo, number {num} is NOT present in the array.");
+
+
+Console.WriteLine("\nTask 35");
+// Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] array4 = new int[123];
+
+int count = 0;
+
+for (int i=0; i<array4.Length; i++)
+{
+    array4[i] = new Random().Next(0, 160);
+    Console.Write(array4[i] + ", ");
+
+    if (array4[i]>=10 && array4[i]<=99)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"\nThere are {count} numbers in the segment between [10, 99].");
+
+
+Console.WriteLine("\n");
+
